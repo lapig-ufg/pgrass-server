bb30756438bc849c622738dfbd725387d5b7541f
Jairo Matos da Rocha <devjairomr@gmail.com>
2022-05-27 09:45:37 -0300

    return points

--- app/api/feature.py
+++ app/api/feature.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import List, Tuple
 
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 
-from app.db import db_features, PyObjectId
+from app.db import MongoModel, db_features, PyObjectId
 from app.model.models import Feature
-
+from app.config import logger
 router = APIRouter()
 
 
@@ -21,3 +22,21 @@ async def get_feature(id:str):
     if (feature := await db_features.find_one({"_id": PyObjectId(id)})) is not None:
         return feature
     raise HTTPException(status_code=404, detail=f"Feature {id} not found")
+
+class Point(MongoModel):
+    point_id:PyObjectId
+    coordinate: Tuple[float,float]
+
+
+@router.get('/points/', response_description="List point in features", response_model=List[Point])
+async def get_features_point():
+    points = []
+    async for feature  in db_features.find():
+        points.append(
+            Point(
+                point_id = PyObjectId(feature['point_id']), 
+                coordinate = (feature['lon'],feature['lat'])
+                )
+            )
+    logger.debug(points)
+    return points
