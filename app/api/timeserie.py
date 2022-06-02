0173019d580acd0a23b2198ef0173262a625309a
Jairo Matos da Rocha <devjairomr@gmail.com>
2022-06-02 11:58:40 -0300

    nova rota de pesquisa

--- app/api/timeserie.py
+++ app/api/timeserie.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, HTTPException
 
 from app.db import db_timeseires, PyObjectId
 from app.model.functions import get_id_by_lon_lat
-from app.model.models import TimeSerie
+from app.model.models import SatelliteEnum, TimeSerie
 
 router = APIRouter()
 
@@ -20,8 +20,30 @@ async def get_timeseires_by_lon_lat(lon:float,lat:float):
         return timeseires
     raise HTTPException(status_code=404, detail=f"Timeseires Point({lon}, {lat}) not found")
 
-@router.get('/{point_id}', response_description="List TimeSeries by point_id", response_model=List[TimeSerie])
+@router.get('/{point_id}', 
+    response_description="List TimeSeries by point_id", 
+    response_model=List[TimeSerie]
+    )
 async def get_timeseires_by_point_id(point_id:str):
     if (timeseires := await db_timeseires.find({"point_id": PyObjectId(point_id)}).to_list(1000)) is not None:
         return timeseires
     raise HTTPException(status_code=404, detail=f"Timeseires point_id {point_id} not found")
+
+
+@router.get(
+    "/{point_id}/{sattelite}/{band_index}", 
+    response_description="Get timeseires by point_id satellite band or index ", 
+    response_model=List[TimeSerie]
+)
+async def get_timeseires_by_point_id_satellite_band_index(
+    point_id:str, 
+    sattelite: SatelliteEnum, 
+    band_index: str
+    ):
+    if (timeseires := await db_timeseires.find({
+        "point_id": PyObjectId(point_id),
+        "sattelite": sattelite,
+        "band_index":band_index
+        }).to_list(1000)) is not None:
+        return timeseires
+    raise HTTPException(status_code=404, detail=f"Timeseires point_id:{point_id}, sattelite:{sattelite}, band_index:{band_index} not found")
