91bdbb10341039c1d40cc5fce58305960183933f
Jairo Matos Da Rocha <devjairomr@gmail.com>
2022-06-12 13:22:08 +0000

    uploado file

--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from pydantic import EmailStr
 from .api import feature
 from .api import timeserie
 
@@ -20,3 +21,26 @@ app.include_router(
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
+
+@app.post("/uploadfiles/")
+async def create_upload_files(
+    first_name:str, 
+    last_name:str, 
+    email:EmailStr, 
+    institution:str,
+    file: UploadFile = File(default=None)
+):
+    try:
+        import geopandas as gpd
+        gdf = gpd.read_file(file.file)
+    except Exception as e:
+        raise HTTPException(status_code=415, detail=str(e))
+    return {
+        "filenames": file.filename,
+        "first_name":first_name, 
+        "last_name":last_name, 
+        "email":email, 
+        "institution":institution,
+        "columns":list(gdf.columns),
+        "job_status":''
+    }
