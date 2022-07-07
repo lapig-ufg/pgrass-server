88809884f29b8bfc08f0f4b6f1ad761ca9bce360
Jairo Matos Da Rocha <devjairomr@gmail.com>
2022-07-07 13:15:33 +0000

    dataset

--- app/api/dataset.py
+++ app/api/dataset.py
@@ -6,11 +6,12 @@ router = APIRouter()
 
 
 @router.get('/', response_description="List all Dataset")
-async def get_features():
+async def get_datasets():
     dataset = await db_dataset.find().to_list(100)
     return dataset
 
 @router.get('/{_id}', response_description="Dataset")
-async def get_features(_id):
-    dataset = await db_dataset.find({'_id': PyObjectId(_id)}).to_list(100)
-    return dataset
+async def get_dataset(_id):
+    if (dataset := await db_dataset.find_one({"_id": PyObjectId(_id)})) is not None:
+        return dataset
+    raise HTTPException(status_code=404, detail=f"Dataset {_id} not found")
