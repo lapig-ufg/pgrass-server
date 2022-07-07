518537bcc92d01817ff63ff2e5e03f1951e0b665
Jairo Matos Da Rocha <devjairomr@gmail.com>
2022-07-07 13:21:48 +0000

    dataset

--- app/api/feature.py
+++ app/api/feature.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 
+
 from fastapi import APIRouter, HTTPException
 
 from app.db import MongoModel, db_features, PyObjectId
@@ -8,17 +9,17 @@ from app.config import logger
 router = APIRouter()
 
 
-@router.get('/', response_description="List all features", response_model=List[Feature])
-async def get_features():
-    features = await db_features.find().to_list(100)
+@router.get('/dataset/{dataset_id}', response_description="List all features db_dataset", response_model=List[Feature])
+async def get_features(dataset_id):
+    features = await db_features.find({'dataset_id':PyObjectId(dataset_id)}).to_list(100)
     return features
 
 
 @router.get(
-    "/{id}", response_description="Get a single feature", response_model=Feature
+    "/{_id}", response_description="Get a single feature", response_model=Feature
 )
-async def get_feature(id:str):
-    if (feature := await db_features.find_one({"_id": PyObjectId(id)})) is not None:
+async def get_feature(_id:str):
+    if (feature := await db_features.find_one({"_id": PyObjectId(_id)})) is not None:
         return feature
     raise HTTPException(status_code=404, detail=f"Feature {id} not found")
 
