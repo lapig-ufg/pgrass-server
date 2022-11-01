1b351fc6f7d6324b31c255f98224bf4869c8b60c
renatowow14 <renatogomessilverio@gmail.com>
2022-11-01 14:10:23 -0300

    Change install.sh on dev environment

--- dev/install.sh
+++ dev/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 FOLDER=~/.ssh
+FOLDER02=data/
 if [ -f "poetry.lock" ]; then
     rm poetry.lock
 fi
-
 if [ -f "pyproject.toml" ]; then
     rm pyproject.toml
 fi
@@ -11,6 +11,12 @@ if [ -d "$FOLDER" ]; then
     cp -rvp ~/.ssh ssh/
 else
     echo "Directory ./ssh does not exist!"
+fi
+if [ -d "$FOLDER02" ]; then
+    echo "Directory data/ exist!"
+else
+    echo "Directory data/ does not exist!"
+    mkdir data/
 fi
 cp -rvp ../poetry.lock .
 
