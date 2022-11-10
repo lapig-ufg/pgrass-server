0988532d89b85769180c54deb0b53e838db1a871
Jairo Matos da Rocha <devjairomr@gmail.com>
2022-11-10 11:12:28 -0300

    auth try

--- app/api/auth.py
+++ app/api/auth.py
@@ -31,25 +31,28 @@ async def create_user(new_user: CreateUser):
 
 @router.post('/login', summary="Create access and refresh tokens for user", response_model=Token)
 async def login_for_access_token(form_data: Login):
-    user = authenticate_user(form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
+    try:
+        user = authenticate_user(form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={
+                "sub": user.username, 
+                'first_name':user.first_name,
+                'last_name':user.last_name ,
+                'email':user.email,
+                'institution':user.institution, 
+                'rememberMe':form_data.rememberMe
+                }, expires_delta=access_token_expires
         )
-    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={
-            "sub": user.username, 
-            'first_name':user.first_name,
-            'last_name':user.last_name ,
-            'email':user.email,
-            'institution':user.institution, 
-            'rememberMe':form_data.rememberMe
-            }, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer","rememberMe":form_data.rememberMe }
+        return {"access_token": access_token, "token_type": "bearer","rememberMe":form_data.rememberMe }
+    except Exception as e:
+        raise HTTPException(500,f'{e}')
 
 @router.post("/token", response_model=Token)
 async def get_token(form_data: OAuth2PasswordRequestForm = Depends()):
